d1b7f85f3d8157a7258cb4bc8422a71e4af4f334
JexXxika <wizcajesse@gmail.com>
2023-04-23 15:57:55 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ *main - Entry pint
+ *Return : Always 0 (success)
+ * **/
 int main(void) {
     puts("Programming is like building a multilingual puzzle");
     return 0;
