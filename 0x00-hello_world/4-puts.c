d2b8f79877e94c1157e901f039c75b0708213b74
JexXxika <wizcajesse@gmail.com>
2023-04-24 13:37:22 -0700

    Task 4 update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main - Entry pint
+ *main - A program  that prints a line with the put function
  *Return : Always 0 (success)
-**/
+*/
 int main(void) {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle")
     return 0;
 }
