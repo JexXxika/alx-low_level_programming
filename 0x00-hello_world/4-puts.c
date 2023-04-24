4e2d59ea5c26c6eb802495040780dcccf8c23dc5
JexXxika <wizcajesse@gmail.com>
2023-04-24 13:43:37 -0700

    Task 4 update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  *Return : Always 0 (success)
 */
 int main() {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
