6913ee0b2e5ca1deaf1357bfff37cb5743d09462
JexXxika <wizcajesse@gmail.com>
2023-05-05 18:05:01 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,23 @@
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * _strncpy - Copies at most the number of bytes from string inputted in  src into dest.           
- * @dest: The buffer storing the string copy.                                                                                        
- * @src: The source string.                                                                                                          
- * @n: The maximum number of bytes to copied from src.                                                                               
- *                                                                                                                                   
- * Return: A pointer to the resulting string dest.                                                                                   
- */                                                                                                                                  
-char *_strncpy(char *dest, char *src, int n)                                                                                         
-{                                                                                                                                    
-        int index = 0, src_len = 0;                                                                                                  
-                                                                                                                                     
-        while (src[index++])                                                                                                         
-                src_len++;                                                                                                           
-                                                                                                                                     
-        for (index = 0; src[index] && index < n; index++)                                                                            
-                dest[index] = src[index];                                                                                            
-                                                                                                                                     
-        for (index = src_len; index < n; index++)                                                                                    
-                dest[index] = '\0';                                                                                                  
-                                                                                                                                     
-        return (dest);                                                                                                               
+#include "main.h"
+/**
+ * _strcmp - comparing the string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
